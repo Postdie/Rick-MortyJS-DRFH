d7bd545fe5b30606c769b153aff12a13676a2688
Postdie <fernandezdiego845@gmail.com>
2022-11-07 12:00:06 -0600

    commit del 07/11/2022 2

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import {modalBody, spinner, creaCard } from '.ul-js';
+
 const urlBase = 'https://rickandmortyapi.com/api/character/';
 
 const loadData = (urlBase, page = 1) => {
@@ -45,17 +47,7 @@ const showCharacters = (personajes) => {
 
     });
 }
-const modalBody = (personaje) => {
-    const div = document.createElement('div');
-    div.classList.add("text-center");
-    let html = ``;
-    html += `<img src="${personaje.image}">`;
-    html += `<p>${personaje.status} - ${personaje.species}</p>`;
-    html += `<p>Ultima ubicacion conocida</p><p>${personaje.origin.name} </p>`;
-    html += `<p>Ha aparecido en ${personaje.episode.lenght} Episodios </p>`;
-    div.innerHTML = html;
-    return div;
-}
+
 const showCharactersById = (id) => {
     const urlId = `${urlBase}${id}`;
     fetch(urlId)
@@ -66,24 +58,7 @@ const showCharactersById = (id) => {
             modalContent.appendChild(modalBody(character));
         });
 }
-const creaCard = (personaje) => {
-    const html = `
-    <div class="card bg-dark text-light border" style="width: 18rem;">
-  <img src="${personaje.image}" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">${personaje.name}</h5>
-    <p class="card-text">${personaje.status}</p>
-    <a href="#" 
-    class="btn btn-primary"
-     data-bs-toggle="modal"
-     data-bs-target="#exampleModal"
-     data-id="${personaje.id}">Ver mas</a>
-  </div>
-</div>
-;`
-    return html;
-}
-const navegacion = (e) => {
+function navegacion(e) {
     if (e.target.classList.contains('btn')) {
         const id = e.target.getAttribute('data-id');
         loadData(urlBase, id);
@@ -105,17 +80,7 @@ const loadInfo = (e) => {
         }, 3000);
     }
 }
-const spinner = () => {
-    const div = document.createElement('div');
-    const html =
-        `<div class="d-flex justify-content-center">
-    <div class="spinner-border " role="status">
-      <span class="visually-hidden">Loading...</span>
-    </div>
-  </div>`;
-    div.innerHTML = html;
-    return div;
-}
+
 document.querySelector('#botones').addEventListener('click', navegacion);
 document.querySelector("#characters").addEventListener('click', loadInfo);
 
